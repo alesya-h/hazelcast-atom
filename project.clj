5b0f69f62a426bbcfa03d4714192e38ef49315a7
Alesya Huzik <public@aguzik.net>
2021-11-16 11:07:28 +1100

    update url to github

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject hazelcast-atom "0.1.0-SNAPSHOT"
   :description "Distributed atom implementation on top of hazelcast IMap"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/alesguzik/hazelcast-atom"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.3"]

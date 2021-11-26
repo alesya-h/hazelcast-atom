6df41b921ef8bb63b8c7e6837b733623929011b4
Alesya Huzik <public@aguzik.net>
2021-11-26 15:02:01 +1100

    bump version to 1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.alesguzik/hazelcast-atom "0.1.0-SNAPSHOT"
+(defproject org.clojars.alesguzik/hazelcast-atom "1.0.0-SNAPSHOT"
   :description "Distributed atom implementation on top of hazelcast IMap"
   :url "https://github.com/alesguzik/hazelcast-atom"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
